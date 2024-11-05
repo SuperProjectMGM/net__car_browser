af00666678f40292e87440626114673275c15839
chefxxx <mateusz.mm100@gmail.com>
2024-11-05 17:13:27 +0100

    ID's changes, preparation for second migration

---
+++ search.api/Migrations/20241105161157_New ID's.cs
@@ -0,0 +1,202 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace search.api.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewIDs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rentals_Users_UserId",
+                table: "Rentals");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Rentals_UserId",
+                table: "Rentals");
+
+            migrationBuilder.RenameColumn(
+                name: "PersonalId",
+                table: "Users",
+                newName: "PersonalNumber");
+
+            migrationBuilder.RenameColumn(
+                name: "LicenceId",
+                table: "Users",
+                newName: "LicenceNumber");
+
+            migrationBuilder.RenameColumn(
+                name: "LicenceDate",
+                table: "Users",
+                newName: "DateOfReceiptOfDrivingLicense");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                table: "Users",
+                type: "varchar(255)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Login",
+                table: "Users",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RentalFirmId",
+                table: "Rentals",
+                type: "varchar(255)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                table: "Rentals",
+                type: "varchar(255)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId1",
+                table: "Rentals",
+                type: "varchar(255)",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                table: "Firms",
+                type: "varchar(255)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rentals_UserId1",
+                table: "Rentals",
+                column: "UserId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rentals_Users_UserId1",
+                table: "Rentals",
+                column: "UserId1",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rentals_Users_UserId1",
+                table: "Rentals");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Rentals_UserId1",
+                table: "Rentals");
+
+            migrationBuilder.DropColumn(
+                name: "Login",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "UserId1",
+                table: "Rentals");
+
+            migrationBuilder.RenameColumn(
+                name: "PersonalNumber",
+                table: "Users",
+                newName: "PersonalId");
+
+            migrationBuilder.RenameColumn(
+                name: "LicenceNumber",
+                table: "Users",
+                newName: "LicenceId");
+
+            migrationBuilder.RenameColumn(
+                name: "DateOfReceiptOfDrivingLicense",
+                table: "Users",
+                newName: "LicenceDate");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)")
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn)
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RentalFirmId",
+                table: "Rentals",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)",
+                oldNullable: true)
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Rentals",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)")
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn)
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Firms",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)")
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn)
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rentals_UserId",
+                table: "Rentals",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rentals_Users_UserId",
+                table: "Rentals",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+    }
+}
--- search.api/Models/Rental.cs
+++ search.api/Models/Rental.cs
@@ -4,11 +4,13 @@ public class Rental
 {
     public string Id { get; set; } = string.Empty;
     
-    public int? UserId { get; set; }
-    
     public User? User { get; set; }
     
+    public string UserId { get; set; } = string.Empty;
+    
     public RentalFirm? RentalFirm { get; set; }
+
+    public string RentalFirmId { get; set; } = string.Empty;
     
     public int VinId { get; set; }
     
