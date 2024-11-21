890da6bece4c79916fb762351f03867a51fa525b
Mykhailo Shamrai <mike.shamrai@gmail.com>
2024-11-21 15:45:17 +0100

    version 2.0

--- BrowserFront/src/app/services/cars.service.ts
+++ BrowserFront/src/app/services/cars.service.ts
@@ -2,11 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
-<<<<<<< HEAD
-import { VehicleDetail } from '../cars-list/cars-list.component';
-=======
 import { VehicleDetail } from '../models/VehicleDetail.model';
->>>>>>> 5fc086e23be154686c308ce06bac4d41af9d4d24
 
 @Injectable({
   providedIn: 'root',
@@ -14,13 +10,7 @@ import { VehicleDetail } from '../models/VehicleDetail.model';
 export class CarsService {
   private apiUrl = environment.apiBaseUrl;
 
-<<<<<<< HEAD
-  constructor(private http: HttpClient) {
-    console.log('HttpClient is ready');
-  }
-=======
   constructor(private http: HttpClient) {}
->>>>>>> 5fc086e23be154686c308ce06bac4d41af9d4d24
 
   searchCars(
     rentalFrom: Date,
@@ -33,20 +23,12 @@ export class CarsService {
       );
     }
 
-<<<<<<< HEAD
-    // Konwersja dat na ISO stringi
-=======
->>>>>>> 5fc086e23be154686c308ce06bac4d41af9d4d24
     const params = {
       rentalFrom: rentalFrom.toISOString(),
       rentalTo: rentalTo.toISOString(),
       location: location,
     };
 
-<<<<<<< HEAD
-    // Wysyłanie zapytania do API
-=======
->>>>>>> 5fc086e23be154686c308ce06bac4d41af9d4d24
     return this.http.get<VehicleDetail[]>(`${this.apiUrl}/Search`, { params });
   }
 }
