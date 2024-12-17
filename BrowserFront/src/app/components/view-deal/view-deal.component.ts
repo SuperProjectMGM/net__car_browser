2f513d4cfd43825d6bd915a124b3beb765c67576
Grzesiek1212 <grzegorz.prasek3@gmail.com>
2024-12-18 00:13:28 +0100

    dodanie wyswietlania view detail

--- BrowserFront/src/app/components/view-deal/view-deal.component.ts
+++ BrowserFront/src/app/components/view-deal/view-deal.component.ts
@@ -3,6 +3,8 @@ import { Router } from '@angular/router';
 import { VehicleDetail } from '../../models/VehicleDetail.model';
 import { CommonModule } from '@angular/common';
 import { VehicleToRentService } from '../../services/VehicleToRent.service';
+import { AuthService } from '../../services/auth.service';
+import { ProfileService } from '../../services/profile.service';
 
 @Component({
   selector: 'app-view-deal',
@@ -15,10 +17,13 @@ export class ViewDealComponent {
   car: VehicleDetail | null = null;
   pickupDateTime: Date | null = null;
   returnDateTime: Date | null = null;
+  totalPrice: number = 0;
 
   constructor(
+    private authService: AuthService,
     private vehicleToRentService: VehicleToRentService,
-    private router: Router
+    private router: Router,
+    private profileService: ProfileService
   ) {}
 
   ngOnInit(): void {
@@ -29,6 +34,46 @@ export class ViewDealComponent {
       console.error('No car data found');
       // Opcjonalnie: przekierowanie w razie braku danych
       this.router.navigateByUrl('/cars-list');
+    } else {
+      this.calculateTotalPrice();
+    }
+  }
+
+  rentCar(car: VehicleDetail): void {
+    if (this.authService.isLoggedIn()) {
+      this.profileService.isUserProfileComplete().subscribe((isComplete) => {
+        if (isComplete) {
+          this.authService
+            .wantRentVehicle(car, this.pickupDateTime, this.returnDateTime)
+            .subscribe(
+              (response) => {
+                // Obsługuje udaną odpowiedź
+                console.log('Rent request successful', response); // TODO: co na froncie po wysłanius
+              },
+              (error) => {
+                // Obsługuje błąd
+                console.error('Error during vehicle rent request', error); // TODO: co dalej?
+              }
+            );
+        } else {
+          console.log('Nie ma wypełnionych danych profilu.');
+          this.router.navigate(['/edit-profile']);
+        }
+      });
+    } else {
+      console.log('Użytkownik nie jest zalogowany.');
+      this.router.navigate(['/login']);
+    }
+  }
+
+  calculateTotalPrice(): void {
+    if (this.pickupDateTime && this.returnDateTime && this.car) {
+      const timeDiff = Math.abs(
+        new Date(this.returnDateTime).getTime() -
+          new Date(this.pickupDateTime).getTime()
+      );
+      const days = Math.ceil(timeDiff / (1000 * 60 * 60 * 24)); // Liczba dni
+      this.totalPrice = days * this.car.prize; // Cena całkowita
     }
   }
 }
